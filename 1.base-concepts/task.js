3e2cec2d602705f81674363b2c8bc61eb80ce503
amaslov-QA <83215956+amaslov-QA@users.noreply.github.com>
2022-02-08 11:56:46 +0300

    V1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,21 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let arr = [];
+  let x1 = 0;
+  let x2 = 0;
+  let x3 = 0;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  }
+  else if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    arr.push(x1);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x2);
+  }
+  else if (d === 0) {
+    x3 = (-b) / (2 * a);
+    arr.push(x3)
+  }
+  return arr;
 }
