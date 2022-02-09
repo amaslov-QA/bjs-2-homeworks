984df9223a6c572b6af41f86f7539a281e8f6a2e
amaslov-QA <83215956+amaslov-QA@users.noreply.github.com>
2022-02-09 10:35:33 +0300

    Add files via upload

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,49 @@
+'use strict'
+
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = -100;
+  max =  100;
+  sum = 0;
+  avg = 0;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  getArrayParams(min,max,avg);
 }
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  let sumArr = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sumArr = func(arrOfArr[i]);
+    if (sumArr > max) {
+      max = sumArr
+    }
+  }
   return max;
 }
 
 // Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+let arrParams = getArrayParams(arr);
+
+return arrParams.max - arrParams.min;
